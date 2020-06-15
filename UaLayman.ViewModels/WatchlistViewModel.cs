50fb8c83c66c0136f90ac89da7ae4e642f8578d5
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2020-06-15 23:07:21 +0200

    Show advice when watchlist is empty

--- UaLayman.ViewModels/WatchlistViewModel.cs
+++ UaLayman.ViewModels/WatchlistViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using DynamicData;
+using DynamicData.Aggregation;
+using DynamicData.Binding;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +17,16 @@ namespace UaLayman.ViewModels
     {
         public ObservableCollection<WatchlistItemViewModel> Items { get; } = new ObservableCollection<WatchlistItemViewModel>();
 
+        private ObservableAsPropertyHelper<bool> _isEmpty;
+        public bool IsEmpty => _isEmpty.Value;
+
         public WatchlistViewModel(IScreen screen) : base(screen)
         {
+            Items
+                .ToObservableChangeSet()
+                .Count()
+                .Select(c => c == 0)
+                .ToProperty(this, x => x.IsEmpty, out _isEmpty, true);
         }
     }
 }
